6cf107969685c6367b1ba7b85a558e7d37715baf
xqcherry <h2417185282@outlook.com>
2025-10-16 20:21:54 +0800

    1.无关紧要的小修改

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@ TOKEN=undeGitlab-nxfMzCsmbnsGGxGcDPkY          # 刚才复制的令牌
 CI_SERVER_URL=https://gitlab.unde.site          # 自建实例就换域名
 CI_PROJECT_ID=38                    # 项目首页右下角显示的数字 ID
 PACKAGE_NAME=vscode-fast-cpp              # 自定义包名
-PACKAGE_VERSION=0.0.8                     # 版本号（semver）
-VSIX_FILE=vscode-fast-cpp-0.0.3.vsix               # 本地产物路径
+PACKAGE_VERSION=0.1.0                     # 版本号（semver）
+VSIX_FILE=vscode-fast-cpp-0.1.0.vsix               # 本地产物路径
 # ===========================
 
 curl --header "PRIVATE-TOKEN: ${TOKEN}" \
